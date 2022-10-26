94b269b62be6feebd2b6afb2ee73ae2783d5d5eb
Tiago Albuquerque <104788228+Tigo07@users.noreply.github.com>
2022-10-26 14:45:47 -0300

    Update Robo_Whats.py

--- Robo_Whats.py
+++ Robo_Whats.py
@@ -1,27 +1,70 @@
-import pandas as pd
-contatos_df = pd.read_excel("Enviar.xlsx")
-print(contatos_df)
-
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from playwright.sync_api import Playwright, sync_playwright, expect
 import time
-import urllib
-
-navegador = webdriver.Firefox()
-navegador.get("https://web.whatsapp.com/")
+import pandas as pd
 
 
-while len(navegador.find_elements_by_id("side")) < 1:
-    time.sleep(1)
 
-# já estamos com o login feito no whatsapp web
-for i, mensagem in enumerate(contatos_df['Mensagem']):
-    pessoa = contatos_df.loc[i, "Pessoa"]
-    numero = contatos_df.loc[i, "Número"]
-    texto = urllib.parse.quote(f"Oi {pessoa}! {mensagem}")
-    link = f"https://web.whatsapp.com/send?phone={numero}&text={texto}"
-    navegador.get(link)
-    while len(navegador.find_elements_by_id("side")) < 1:
-        time.sleep(1)
-    navegador.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[2]').send_keys(Keys.ENTER)
-    time.sleep(10)
+#Inicia no navegador
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    # Abre a guia
+    page = context.new_page()
+    page.goto("https://web.whatsapp.com")
+    print("Valide o QRCode em seu Aparelo")
+    page.wait_for_timeout(13000)
+
+    print('iniciando')
+    
+    #Ler a planilha
+    Sheet1_df = pd.read_excel("suaplanilha.xlsx")
+    for i, DADOS in enumerate(Sheet1_df['Nome']):
+        Nome = Sheet1_df.loc[i,"Nome"]
+        Telefone = Sheet1_df.loc[i,"Telefone"]
+        Menssagem =Sheet1_df.loc[i,"Menssagem"]
+
+        print(Nome,Telefone,Menssagem)
+        ##exit()
+        page.wait_for_timeout(13000)
+        #variavel para envio da mensagem
+        text = "&text="
+        
+        #Saudação + nome da planilha
+        msgg = "Olá "+str(Nome)
+        
+        #Mensagem após saudação + nome
+        msg =  " Teste"
+
+        #Link já com o número e mensagens prontas pra envio (unifica todas as variaveis anteriores
+        link = "https://web.whatsapp.com/send?phone="+str(Telefone)+str(text)+str(msgg)+str(msg)
+              
+
+        #Aqui é um ponto importante, ele vai tentar realizar o envio, caso não consegui vai para o proximo caso da planilha.         
+        try:
+
+            #aqui ele irá usar a guia já existente indo de item a item até finalizar planilha
+            page.goto(link)
+
+            #Aqui ele aguarda o elemento da barra de pesquisa, para ter certeza que a tela carregou. (se der erro vai para o proximo caso)
+            with page.expect_navigation():# aguarda o elemento por 30 segundos por padrão
+                 page.locator("div[role=\"textbox\"]")
+
+            # Se tudo der certo nas etapas anteriores, ele vai aguardar o botão de envio ficar disponivel e pertar para enviar. 
+            with page.expect_navigation(50):# aguarda o elemento por 30 segundos por padrão
+                 page.locator("[data-testid=\"conversation-panel-wrapper\"] button").nth(4).click()
+                 print("msn Enviada")
+                 
+            #Após o envio aguarda 10 segundos para ter certeza que a mensagem foi enviada. 
+            time.sleep(10)
+
+        #Caso as etapas anteriores do Try (Tente) derem errado ele vai para o proximo item da planilha, mas você pode colocar a saida que achar melhor.     
+        except:
+            continue 
+                    
+    # ---------------------
+    #context.close()
+    #browser.close()
+with sync_playwright() as playwright:
+    run(playwright)
+
+print('acabou')
